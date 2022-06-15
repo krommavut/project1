12fbae6d2bc21861aa00155b068ade4b9a5a249c
kromma <krommavut@gmail.com>
2022-06-15 18:53:00 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE=5;
+            const float G=9.8f;
+            Console.WriteLine("Hello World!");
+            int year =1990;
+            string name ="Thomas";
+            string nationality ="Thai";
+
+
+            Console.WriteLine("He was born {0}",year);
+            Console.WriteLine("He is {0}",nationality);
+            Console.WriteLine("G is {0},{0}",SIZE,G);
+        }
+    }
+}
